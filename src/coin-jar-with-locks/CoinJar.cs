c8e8c97357aa868b37645fd699a3949ea25fdc2f
Kiran <kiranjagz@gmai.com>
2023-06-27 15:37:43 +0200

    feat: added using locks

---
+++ src/coin-jar-with-locks/CoinJar.cs
@@ -0,0 +1,43 @@
+﻿using coin_jar.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coin_jar_with_locks
+{
+    internal class CoinJar : ICoinJar
+    {
+        private decimal _totalBalance;
+        private readonly object lockObject = new object();
+
+        public CoinJar()
+        { 
+            _totalBalance = 0;
+        }
+        public void AddCoin(decimal coin)
+        {
+            lock(lockObject)
+            {
+                _totalBalance += coin;
+            }
+        }
+
+        public void EmptyJar()
+        {
+            lock (lockObject)
+            {
+                _totalBalance = 0;
+            }
+        }
+
+        public decimal GetCoinBalance()
+        {
+            lock (lockObject)
+            {
+                return _totalBalance;
+            }
+        }
+    }
+}
---
+++ src/coin-jar-with-locks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace coin_jar_with_locks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coinJar = new CoinJar();
+
+            Thread threadOne = new Thread(() => coinJar.AddCoin(0.25m));
+            Thread threadTwo = new Thread(() => coinJar.AddCoin(0.25m));
+            Thread threadThree = new Thread(() => coinJar.AddCoin(0.25m));
+
+            threadOne.Start();
+            threadTwo.Start();
+            threadThree.Start();
+
+            threadOne.Join();
+            threadTwo.Join();
+            threadThree.Join();
+
+            var totalBalance = coinJar.GetCoinBalance();
+
+            Console.WriteLine(totalBalance);
+
+            coinJar.EmptyJar();
+        }
+    }
+}
